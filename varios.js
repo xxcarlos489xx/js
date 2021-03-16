a0be3b02af4f14267e094e00c9a170b119d1a0b6
Carlos Villanueva <xxcarlos489xx@gmail.com>
2021-03-16 16:37:53 -0500

    nuevos

--- varios.js
+++ varios.js
@@ -78,18 +78,6 @@ for (var i = 0; i < b.length; i++ ) {
 	console.log(c[i].textContent);
 }
 
-/*=============================================================*/
-/*=============================================================*/
-
-var lista = document.getElementsByClassName('app-class');
-
-Array.from(lista).forEach(function($valor){
-	console.log($valor.textContent);
-});
-/*=============================================================*/
-/*=============================================================*/
-
-
 $("input[name='textotest']").change(function() {
 
 	alert("holi");
@@ -174,8 +162,6 @@ $('#selectDistFirmaVen').prop('selectedIndex',0);
 '<a href="javascript:history.back()" class="text-muted">'
 ////////////ABRIR EN UNA VENTANA NUEVA FLOTANTE/////////////////////
 'onclick="window.open(this.href, this.target, "width=300,height=400"); return false;"';
-
-
 /*=============================================
 VALIDACIÓN FORMULARIO CONTACTENOS
 =============================================*/
@@ -227,8 +213,6 @@ function validarContactenos() {
 
     return true;
 }
-
-
 /*=============================================
 			AGREGAR TEMPORIZADOR
 =============================================*/
@@ -259,3 +243,97 @@ function verificar() {
             OBTENER FILE DE UN INPUT
 =============================================*/
 var pdf = $("#uploadPDF1").prop("files")[0];
+/*==================================================================
+                ATRIBUTOS DEL DOM A ARRAY (creando array)
+====================================================================*/
+var lista = document.getElementsByClassName('app-class');
+Array.from(lista).forEach(function(valor){
+	console.log(valor.textContent);
+});
+/*==================================================================
+        Array.isArray(nameArray) (DEVUELVE TFALSE SI NO ES ARRAY)
+		MAP (PERMITE MODIFICAR EL CONTENIDO DEL ARRAY)
+        FILTER (filtra de acuerdo a la condicion y devuelve el resto)
+        FIND (busca y retorno solo un elemento)
+        CONCAT (CONCATENA ARRAYS)
+        kEYS (DEVUELVE EL INDEX DEL ARRAY)
+        JOIN (CONVIERTE A STRING LOS VALORES DE UN ARRAY) 
+        SPLITE
+        FINDINDEX (DEVUELVE EL INDICE DEL ELEMENTO BUSCADO)
+        LASTINDEXIF (BUSCA DE DERECHA A IZQUIERDA)
+        SOME (SI ALGUN ELEMENTO CUMPLE LA CONDICION, DEVUELVE TRUE OR FALSE)
+        EVERY (APLICA UNA CONDICION SOBRE TODO LOS OBJETOS Y DEVUELVE UN BOOLEANO)
+        REDUCE (USA UN ACUMULADOR)
+====================================================================*/
+const data = [
+                {
+                    "id": "4a68ddcd-f856-41c9-96b1-51573caefa4f",
+                    "titulo": "Paquete seminario 2021",
+                    "precio_nosuscriptor": "150.00",
+                    "tipo": 0
+                },
+                {
+                    "id": "4a68ddcd-f856-41c9-96b1-51573caefa4f",
+                    "titulo": "Paquete seminario 2019",
+                    "precio_nosuscriptor": "150.00",
+                    "tipo": 1
+                },
+                {
+                    "id": "4a68ddcd-f856-41c9-96b1-51573caefa4f",
+                    "titulo": "Paquete taller 2021",
+                    "precio_nosuscriptor": "150.00",
+                    "tipo": 1
+                }
+            ],
+const onlyOne = data.find(element =>{
+    return element.titulo == "2021";//devuelve el primer elemento encontrado
+})
+const onlyOne = data.findIndex(element =>{
+    return element.titulo == "2021";//devuelve el indice
+})
+const globalBusqueda = data.filter(element =>{
+    //return element.titulo == "2021";//busqueda restringida
+    return (/2021/g).test(element.titulo);//busqueda mas global
+})
+//SE AGREGA UN NUEVO PRECIO, NO MODIFICANDO EL OBJETO PRINCIPAL(data)
+const newData = data.map(element =>{
+    let newPrice = element.precio_suscriptor + element.precio_suscriptor*0.18;
+    return {...element, precio_suscriptor: newPrice}
+})
+const everyElement = data.every(element =>{
+    return element.precio >= 50;//DEVUELVE TRUE OR FALSE
+})
+const oneElement = data.some(element =>{
+    return element.precio >= 50;//ANALIZA SOLO UN ELEMENTO
+})
+const total = data.reduce((total,element) =>{
+    return total+=parseInt(element.precio_nosuscriptor);
+},0)
+
+var arr1 = [1,2,3,4]
+var arr2 = [4,5,6,7]
+const total = arr1.reduce((acum,item) =>{//[1,2,3,4]
+    return Math.max(acum,item);//retorna numero mayor
+})
+const total = arr1.reduce((acum,item) =>{//[1,2,3,4]
+    return Math.min(acum,item);//retoran numero menor
+})
+Array.isArray(arr1)//DEVUELVE TRUE, PORQUE EL ARR1 ES UN ARRAY
+
+var ex1 = arr1.concat(arr2)//UNE DOS ARRAYS [0, 1, 2, 3, 4, 5, 6, 7]
+var ex2 = ex1.concat(2)//TRAE A PARTIR DEL TERCER INDICE
+var ex3 = ex1.concat(2,5)//TRAE A PARTIR DEL TERCER INDICE, HASTA ANTES DEL QUINTO INDICE 2<X<5
+var ex4 = ex1.concat(-1)//TRAE ULTIMO INDICE
+var ex5 = ex1.concat(-3)//TRAE ULTIMOS TRES INDICES
+var ex6 = arr1.join()// por defecto es una coma 0,1,2,3
+var ex6 = arr1.join('/')// 0/1/2/3
+var ex7 = [...arr1.keys()];// devuelve los indices de un array [0, 1, 2, 3]
+var ex8 = arr1.reverse();// reordena un array [3, 2, 1, 0]
+arr.sort((a,b)=>{
+    return b-a; //ORDENA DE MENOR A MAYOR Y VICEVERSA
+})
+
+
+
+
+
